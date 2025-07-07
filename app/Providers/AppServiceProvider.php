c6f2ebdca3c13b6af50700b7b05b759a0a8f1d1a
dfcastro <dfcastro@outlook.com.br>
2025-07-07 20:11:55 -0300

    modificando provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL; // <-- Importa a classe URL
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Garante que em produção (na Fly.io) todos os links usem https
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
